8ab7df1f7321df7ead04706375577ae2cfa629c0
unknown <sujoydaspc2023@gmail.com>
2025-01-01 23:54:43 +0600

    Nav Style and hero

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,17 +7,17 @@ const Header = () => {
           <div id="nav-logo" className="">
             <img src="/public/assets/Header/navlogo.png" alt="" />
           </div>
-          <div id="nav-menu" className=" flex justify-center items-center gap-4">
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'/about'}>About Us</NavLink>
-            <NavLink to={'/services'}>Services</NavLink>
-            <NavLink to={'/blogs'}>Blogs</NavLink>
-            <NavLink to={'/projects'}>Projects</NavLink>
-            <NavLink to={'/page'}>Page</NavLink>
-            <NavLink to={'/contact'}>Contact Us</NavLink>
+          <div id="nav-menu" className=" flex justify-center items-center gap-10">
+            <NavLink to={'/'} className="active:scale-95 hover:text-[#FF9900] transition-all">Home</NavLink>
+            <NavLink to={'/about'} className="active:scale-95 hover:text-[#FF9900] transition-all">About Us</NavLink>
+            <NavLink to={'/services'} className="active:scale-95 hover:text-[#FF9900] transition-all">Services</NavLink>
+            <NavLink to={'/blogs'} className="active:scale-95 hover:text-[#FF9900] transition-all">Blogs</NavLink>
+            <NavLink to={'/projects'} className="active:scale-95 hover:text-[#FF9900] transition-all">Projects</NavLink>
+            <NavLink to={'/page'} className="active:scale-95 hover:text-[#FF9900] transition-all">Page</NavLink>
+            <NavLink to={'/contact'} className="active:scale-95 hover:text-[#FF9900] transition-all">Contact Us</NavLink>
             <NavLink to={'/'} className="flex gap-2">
-              <img src="/public/assets/Header/call.png" alt="" />
-              <span>+01303436299</span>
+              <img className="w-6 active:scale-95 transition-all" src="/public/assets/Header/call.png" alt="" />
+              <span className="active:scale-95 hover:text-[#FF9900] transition-all">+01303436299</span>
             </NavLink>
           </div>
       </nav>
